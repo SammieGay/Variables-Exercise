9ec28f408cdea25057d98a41c7475a8bd2583908
Sammie Gay <smylezstudioz@gmail.com>
2023-09-22 18:23:01 -0500

    Fixed exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declare and initialize variables
+            string name = "Sammie";
+            int age = 39;
+            char middleInitial = 'L';
+            bool isOver18 = true;
+            double currentTemp = 90.5;
+            decimal currentPrice = 29.99m;
+
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
+
         }
     }
 }
